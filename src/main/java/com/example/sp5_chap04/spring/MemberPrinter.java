5cb385dd07ea76475ca6736beb9a4d40c5d39a1c
SYU <xeungjin@gmail.com>
2025-03-27 15:45:31 +0900

    ~ 125 page, 생성자 ofPattern() 추가

--- src/main/java/com/example/sp5_chap04/spring/MemberPrinter.java
+++ src/main/java/com/example/sp5_chap04/spring/MemberPrinter.java
@@ -8,6 +8,10 @@ import java.util.Optional;
 public class MemberPrinter {
     private DateTimeFormatter dateTimeFormatter;
 
+    public MemberPrinter() {
+        dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");
+    }
+
     public void print(Member member) {
         if(dateTimeFormatter == null) {
             System.out.printf("회원 정보 : 아이디=%d, 이메일=%s, 이름=%s, 등록일=%tF\n", member.getId(), member.getEmail(), member.getName(), member.getRegisterDateTime());
@@ -17,12 +21,8 @@ public class MemberPrinter {
         }
     }
 
-    @Autowired
-    public void setDateTimeFormatter(Optional<DateTimeFormatter> formatterOpt) {
-        if(formatterOpt.isPresent()) {
-            this.dateTimeFormatter = formatterOpt.get();
-        }else {
-            this.dateTimeFormatter = null;
-        }
+    @Autowired(required = false)
+    public void setDateTimeFormatter(DateTimeFormatter dateTimeFormatter) {
+        this.dateTimeFormatter = dateTimeFormatter;
     }
 }
