963db5a2a20d0e0f6cbd9390c45ebf7bee496b3f
SYU <xeungjin@gmail.com>
2025-03-27 12:32:03 +0900

    Chapter 04. Initial commit

---
+++ src/main/java/com/example/sp5_chap04/config/AppCtx.java
@@ -0,0 +1,53 @@
+package com.example.sp5_chap04.config;
+
+import com.example.sp5_chap03.spring.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppCtx {
+
+    @Bean
+    public MemberDao memberDao() {
+        return new MemberDao();
+    }
+
+    @Bean
+    public MemberRegisterService memberRegSvc() {
+        return new MemberRegisterService(memberDao());
+    }
+
+    @Bean
+    public ChangePasswordService changePwdSvc() {
+        ChangePasswordService pwdSvc = new ChangePasswordService();
+        pwdSvc.setMemberDao(memberDao());
+        return pwdSvc;
+    }
+
+    @Bean
+    public MemberPrinter memberPrinter() {
+        return new MemberPrinter();
+    }
+
+    @Bean
+    public MemberListPrinter listPrinter() {
+        return new MemberListPrinter(memberDao(), memberPrinter());
+    }
+
+    @Bean
+    public MemberInfoPrinter infoPrinter() {
+        MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
+        infoPrinter.setMemberDao(memberDao());
+        infoPrinter.setPrinter(memberPrinter());
+
+        return infoPrinter;
+    }
+
+    @Bean
+    public VersionPrinter versionPrinter() {
+        VersionPrinter versionPrinter = new VersionPrinter();
+        versionPrinter.setMajorVersion(5);
+        versionPrinter.setMinorVersion(0);
+        return versionPrinter;
+    }
+}
---
+++ src/main/java/com/example/sp5_chap04/spring/WrongIdPasswordException.java
@@ -0,0 +1,4 @@
+package com.example.sp5_chap04.spring;
+
+public class WrongIdPasswordException extends RuntimeException {
+}
